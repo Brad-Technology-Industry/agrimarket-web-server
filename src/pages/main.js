87ac956c9f810539630eee895eac8305b01383a5
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-22 19:07:36 +0100

    feat(app): changed the mode of husky command and tested

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,20 +1,11 @@
-const openMenu = document.querySelector('#openMenu'); 
+const openMenu = document.querySelector('#openMenu');
 const navBarMobile = document.querySelector('#navBarMobile');
 const closeMenu = document.querySelector('#closeMenu');
 
+openMenu.addEventListener('click', () => {
+   navBarMobile.classList.toggle('hidden');
+});
 
-
-openMenu.addEventListener('click', ()=>{
-  
-navBarMobile.classList.toggle('hidden')
-    
-
-
-})
-
-closeMenu.addEventListener('click', ()=>{
-  
-
-    navBarMobile.classList.toggle('hidden')
-
-})
+closeMenu.addEventListener('click', () => {
+   navBarMobile.classList.toggle('hidden');
+});
